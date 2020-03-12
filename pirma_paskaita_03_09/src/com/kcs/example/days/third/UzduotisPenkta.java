53c7b38ea354d73f3da5d48c8ec7f6def54eee56
andriusbaltrunas <andriusbaltrunas@yahoo.com>
2020-03-12 19:04:21 +0200

    trecia diena penkta uzduotis

---
+++ pirma_paskaita_03_09/src/com/kcs/example/days/third/UzduotisPenkta.java
@@ -0,0 +1,34 @@
+package com.kcs.example.days.third;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UzduotisPenkta {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        UzduotisPenkta uzduotisPenkta = new UzduotisPenkta();
+        System.out.println("Ivesk nuvaziuota atstuma");
+        double atstumas = uzduotisPenkta.getValue(sc);
+        System.out.println("Ivesk kuro sanaudas");
+        double sanaudos = uzduotisPenkta.getValue(sc);
+        System.out.println("Vidurkis yra: " + uzduotisPenkta.average(atstumas, sanaudos));
+
+    }
+
+    private double average(double nuvaziuotasAtstumas, double kuroSanaudos) {
+        return (kuroSanaudos * 100) / nuvaziuotasAtstumas;
+    }
+
+    private double getValue(Scanner sc) {
+        Double value = null;
+        while (value == null) {
+            try {
+                value = sc.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("Neteisinga reiksme, pakartokite");
+                sc.nextLine();
+            }
+        }
+        return value;
+    }
+}
