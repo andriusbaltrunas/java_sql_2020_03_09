a9fdfdbc33f9cdf12fc000960120e0c2213c598c
andriusbaltrunas <andriusbaltrunas@yahoo.com>
2020-03-13 20:34:12 +0200

    code converter

--- pirma_paskaita_03_09/src/com/kcs/example/days/fife/AJCodeConverter.java
+++ pirma_paskaita_03_09/src/com/kcs/example/days/fife/AJCodeConverter.java
@@ -3,13 +3,40 @@ package com.kcs.example.days.fife;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class AJCodeConverter {
     public static void main(String[] args) {
+        Map<String, String> code = readCode();
+        StringBuilder sb = new StringBuilder();
+        for (String item : readLetter()) {
+            String translation = code.get(item);
+            if("tarpas".equals(translation)){
+                translation = " ";
+            }
+            sb.append(translation);
+        }
 
+        System.out.println("Isverstas laisvas");
+        System.out.println(sb.toString());
+    }
+
+    private static Map<String, String> readCode() {
+        Map<String, String> code = new HashMap<>();
+
+        try (BufferedReader bf = new BufferedReader(new FileReader("code.txt"))) {
+            String line;
+            while ((line = bf.readLine()) != null) {
+                String[] items = line.split(" ");
+                if (items.length == 2) {
+                    code.put(items[0], items[1]);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Cannot read code");
+        }
+
+        return code;
     }
 
     private static List<String> readLetter() {
